dcb36105e0578840971f8cdb3ce1dded7144079d
tanjimhasan <tanjim.hasan.pro@gmail.com>
2021-01-11 12:37:49 +0600

    commit updated

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -561,7 +561,9 @@
         var columnLeft = $(".atbd_col_left.col-lg-8");
         var columnRight = $(".directorist.col-lg-4");
         var tabColumn = $(".atbd_dashboard_wrapper .tab-content .tab-pane .col-lg-4");
+        var containerDiv = $('#directorist.atbd_wrapper .container-fluid');
         var w_size = d_wrapper.width();
+        var containerWidth = containerDiv.width();
         if (w_size >= 500 && w_size <= 735) {
             columnLeft.toggleClass("col-lg-8");
             columnRight.toggleClass("col-lg-4");
@@ -569,6 +571,10 @@
         if (w_size <= 600) {
             d_wrapper.addClass("size-xs");
             tabColumn.toggleClass("col-lg-4");
+        }
+
+        if(containerWidth > 1199){
+            containerDiv.toggleClass('container-fixed');
         }
 
         var listing_size = $(".atbd_dashboard_wrapper .atbd_single_listing").width();
--- templates/public-templates/shortcodes/forms/fields/listing-type.php
+++ templates/public-templates/shortcodes/forms/fields/listing-type.php
@@ -10,19 +10,24 @@ $description = get_directorist_option('featured_listing_desc');
     <?php $listing_type = !empty($listing_info['listing_type']) ? $listing_info['listing_type'] : ''; ?>
 
     <h4 class="atbdp_option_title"><?php _e('Choose Listing Type', 'directorist-pricing-plans') ?><span class="atbdp_make_str_red"> *</span></h4>
-    <div class="atbdp_input_group --atbdp_inline">
-        <input id="general" type="radio" class="atbdp_radio_input" <?php echo ($listing_type == 'general') ? 'checked' : ''; ?> name="listing_type" value="general">
-        <label for="general" class="general_listing_type_select">
-            <?php echo esc_attr( $data['general_label'] ); ?>
-        </label>
-    </div>
-    <div class="atbdp_input_group --atbdp_inline">
-        <input id="featured" type="radio" class="atbdp_radio_input" <?php echo ($listing_type == 'featured') ? 'checked' : ''; ?> name="listing_type" value="featured">
-        <label for="featured" class="featured_listing_type_select">
-            <?php echo esc_attr( $data['featured_label'] ); ?>
-            <small class="atbdp_make_str_green"><?php
-            echo esc_attr( $description ) ;?>
-            </small>
-        </label>
+    <div class="directorist_type-selection-wrap">
+        <div class="atbdp_input_group --atbdp_inline">
+            <input id="general" type="radio" class="atbdp_radio_input" <?php echo ($listing_type == 'general') ? 'checked' : ''; ?> name="listing_type" value="general">
+            <label for="general" class="general_listing_type_select">
+                <span><?php echo esc_attr( $data['general_label'] ); ?></span>
+                <span class="directorist_text-extra">5 of 20 Listings available</span>
+            </label>
+        </div>
+        <div class="atbdp_input_group --atbdp_inline">
+            <input id="featured" type="radio" class="atbdp_radio_input" <?php echo ($listing_type == 'featured') ? 'checked' : ''; ?> name="listing_type" value="featured">
+            <label for="featured" class="featured_listing_type_select">
+                <span><?php echo esc_attr( $data['featured_label'] ); ?></span>
+                <span class="directorist_text-extra">
+                    <small class="atbdp_make_str_green"><?php
+                    echo esc_attr( $description ) ;?>
+                    </small>
+                </span>
+            </label>
+        </div>
     </div>
 </div>
