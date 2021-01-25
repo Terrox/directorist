b1766512fec69b4917d5452f801f73b51daf14ae
Syed Galib <aazztech13@gmail.com>
2021-01-25 18:59:16 +0600

    improved asset enqueue class

--- includes/classes/class-enqueue-assets.php
+++ includes/classes/class-enqueue-assets.php
@@ -1,7 +1,6 @@
 <?php
-
-if ( ! class_exists( 'ATBDP_Enqueue_Assets' ) ):
-class ATBDP_Enqueue_Assets {
+namespace Directorist;
+class Enqueue_Assets {
 
     public $js_scripts  = [];
     public $css_scripts = [];
@@ -10,24 +9,8 @@ class ATBDP_Enqueue_Assets {
      * Constuctor
      */
     function __construct() {
-        // Vendor
-        $this->add_vendor_css_scripts();
-        $this->add_vendor_js_scripts();
-
-        // Public
-        $this->add_public_css_scripts();
-        $this->add_public_js_scripts();
-
-        // Admin
-        $this->add_admin_css_scripts();
-        $this->add_admin_js_scripts();
-
-        // Global
-        $this->add_global_css_scripts();
-        $this->add_global_js_scripts();
-        
-        // Apply Hook to Scripts
-        add_action( 'init', [ $this, 'apply_hook_to_scripts' ] );
+        // Load Assets
+        add_action( 'init', [ $this, 'load_assets'] );
 
         // Enqueue Public Scripts
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_public_scripts' ] );
@@ -39,8 +22,43 @@ class ATBDP_Enqueue_Assets {
         add_action( 'init', [ $this, 'enqueue_global_scripts' ] );
     }
 
+    /**
+     * Load Assets
+     *
+     * @return void
+     */
+    public function load_assets() {
+        // Load Vendor Assets
+        $this->add_vendor_css_scripts();
+        $this->add_vendor_js_scripts();
 
-    // apply_hook_to_scripts
+        // Load Public Assets
+        $this->add_public_css_scripts();
+        $this->add_public_js_scripts();
+
+        // Load Admin Assets
+        $this->add_admin_css_scripts();
+        $this->add_admin_js_scripts();
+
+        // Load Global Assets
+        $this->add_global_css_scripts();
+        $this->add_global_js_scripts();
+
+        // Inject Scripts Meta
+        $this->inject_scripts_meta();
+
+        atbdp_console_log( $this->js_scripts );
+
+        // Apply Hook to Scripts
+        $this->apply_hook_to_scripts();
+    }
+
+
+    /**
+     * Apply Hook to Scripts
+     *
+     * @return void
+     */
     public function apply_hook_to_scripts() {
         $this->css_scripts = apply_filters( 'directorist_css_scripts', $this->css_scripts );
         $this->js_scripts = apply_filters( 'directorist_js_scripts', $this->js_scripts );
@@ -410,5 +428,40 @@ class ATBDP_Enqueue_Assets {
 
         return $file_name;
     }
+
+    /**
+     * Inject Scripts Meta
+     *
+     * @return void
+     */
+    public function inject_scripts_meta() {
+        // Add js script meta
+
+        foreach( $this->js_scripts as $handle => $script_args ) {
+            // Inject WP dependency meta
+            if (  empty( $script_args['file_name'] ) ||  empty( $script_args['group'] ) ) {
+                continue;
+            }
+
+            $file_name = $this->get_script_file_name( $script_args );
+            $asset_path = ATBDP_DIR . "assets/dest/{$script_args['group']}/js/{$file_name}.asset.php";
+
+            if ( ! file_exists( $asset_path ) ) { continue; }
+            $asset_source = require( $asset_path );
+
+            $deps = ( isset( $script_args['deps'] ) && is_array( $script_args['deps'] ) ) ? $script_args['deps'] : [];
+            $ver  = ( isset( $script_args['ver'] ) && is_string( $script_args['ver'] ) ) ? $script_args['ver'] : '';
+
+            if ( isset( $asset_source['dependencies'] ) ) {
+                $deps = array_merge( $deps, $asset_source['dependencies'] );
+            }
+
+            if ( isset( $asset_source['version'] ) ) {
+                $ver =  $asset_source['version'];
+            }
+
+            $this->js_scripts[ $handle ][ 'deps' ] = $deps;
+            $this->js_scripts[ $handle ][ 'ver' ]  = $ver;
+        }
+    }
 }
-endif;
